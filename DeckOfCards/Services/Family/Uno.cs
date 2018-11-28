f22d662165cee58e1e82aec236447a309e30782e
Robert Williams <bobw2829@gmail.com>
2018-11-27 20:06:27 -0600

    Add files via upload

---
+++ DeckOfCards/Services/Family/Uno.cs
@@ -0,0 +1,23 @@
+﻿using Business.Models.DeckOfCards;
+using DeckOfCards.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeckOfCards.Services.Family
+{
+    public class Uno : CardGame
+    {
+        public Uno(IDeck deck) :base(deck)
+        {
+            Name = "Uno";
+        }
+
+        public override List<CardModel> Deal()
+        {
+            return Deck.Cards.GetRange(0, 11);
+        }
+    }
+}
