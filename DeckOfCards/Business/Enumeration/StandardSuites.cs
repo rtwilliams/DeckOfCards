7099cf4be30ebd48a321100f3d5481948c55132c
Robert Williams <bobw2829@gmail.com>
2018-11-27 20:03:30 -0600

    Add files via upload

---
+++ DeckOfCards/Business/Enumeration/StandardSuites.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeckOfCards.Business.Enumerations
+{
+    public enum StandardSuites
+    {
+        Spade,
+        Diamond,
+        Heart,
+        Club
+    }
+}
