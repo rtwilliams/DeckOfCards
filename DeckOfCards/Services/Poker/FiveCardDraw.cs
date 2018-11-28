9f35a0a6dae5c7b07facc6bfb33b9d91e16800f3
Robert Williams <bobw2829@gmail.com>
2018-11-27 20:06:59 -0600

    Add files via upload

---
+++ DeckOfCards/Services/Poker/FiveCardDraw.cs
@@ -0,0 +1,18 @@
+﻿using Business.Models.DeckOfCards;
+using DeckOfCards.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeckOfCards.Services.Poker
+{
+    public class FiveCardDraw : CardGame
+    {
+        public FiveCardDraw(IDeck deck) : base(deck)
+        {
+            Name = "Five Card Draw";
+        }
+    }
+}
